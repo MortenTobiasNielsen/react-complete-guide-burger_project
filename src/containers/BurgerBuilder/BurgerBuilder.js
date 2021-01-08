90bd1db6d0b2cf3fd85e02b9d80aa9062bd4c306
Morten <mtnielsen89@gmail.com>
2021-01-08 17:35:07 +0100

    Initial layout structure

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class BurgerBuilder extends Component {
+    render () {
+        return (
+            <React.Fragment>
+                <div>
+                    Burger
+                </div>
+                <div>
+                    Build Controls
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
