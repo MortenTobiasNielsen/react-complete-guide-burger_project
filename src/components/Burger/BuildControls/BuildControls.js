215e6efcdf6544de58bd8446b603de46cbfcc818
Morten <mtnielsen89@gmail.com>
2021-01-10 17:02:02 +0100

    Changed order of ingredients in build controls to match the modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,9 +5,10 @@ import BuildControl from "./BuildControl/BuildControl"
 
 const controls = [
     { label: "Salad", type: "salad"},
+    { label: "Bacon", type: "bacon"},
     { label: "Cheese", type: "cheese"},
     { label: "Meat", type: "meat"},
-    { label: "Bacon", type: "bacon"},
+  
 ]
 
 const buildControls = (props) => {
