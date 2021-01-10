4494f3e0aa5765f5bc04848f85da55a36a6cfcad
Morten <mtnielsen89@gmail.com>
2021-01-10 17:31:42 +0100

    Added backdrop to the modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true})
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
     render () {
         const disableInfo ={
             ...this.state.ingredients
@@ -98,7 +102,9 @@ class BurgerBuilder extends Component {
 
         return (
             <React.Fragment>
-                <Modal show={this.state.purchasing}>
+                <Modal 
+                show={this.state.purchasing}
+                modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
