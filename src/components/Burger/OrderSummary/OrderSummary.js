d248e44790f438b5d090cad76c19eaeb878eac7d
Morten <mtnielsen89@gmail.com>
2021-01-11 14:27:03 +0100

    Moved layout and changed imports

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Button from "../../UI/Button/Button"
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functional component, doesn't need to be a class
+    componentDidUpdate() {
+        console.log("[orderSummary] Did update" )
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: "capitalize"}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>
             )
         })
-    
-    return(
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-             <ul>
-                {ingredientSummary}
-             </ul>
-             <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-             <p> Continue to Checkout?</p>
-             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-             <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-        </React.Fragment>
-    )
+
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p> Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </React.Fragment>
+        )
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
