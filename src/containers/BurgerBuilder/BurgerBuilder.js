ea4484f4a6e6cc3a8b73306fbd3bcdaa7f603041
Morten <mtnielsen89@gmail.com>
2021-01-13 08:58:13 +0100

    Added axois post for orders

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,8 +2,9 @@ import React, {Component} from "react";
 
 import Burger from "./../../components/Burger/Burger";
 import BuildControls from "./../../components/Burger/BuildControls/BuildControls";
-import Modal from "../../components/UI/Modal/Modal"
-import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary"
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -92,7 +93,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        //alert("You continue!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Morten Nielsen",
+                address: {
+                    street: "test street 1",
+                    zipcode: "2870",
+                    country: "Denmark",
+                },
+                email: "test@test.com",
+            },
+            deliveryMethod: "fastest",
+        }
+
+        axios.post("/orders.json", order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
