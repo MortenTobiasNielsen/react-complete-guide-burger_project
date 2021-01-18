0d32f6cb35696e96212b3d9aabae1ea153741110
Morten <mtnielsen89@gmail.com>
2021-01-18 14:18:01 +0100

    Added form submission

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -57,7 +57,7 @@ class ContactData extends Component {
                         {value: "cheapest", displayValue: "Cheapest"},
                     ]
                 },
-                value: "",
+                value: "Fastest",
             },
         },
         loading: false,
@@ -67,19 +67,16 @@ class ContactData extends Component {
         event.preventDefault();
 
         this.setState({loading: true})
+
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        };
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: "Morten Nielsen",
-                address: {
-                    street: "test street 1",
-                    zipcode: "2870",
-                    country: "Denmark",
-                },
-                email: "test@test.com",
-            },
-            deliveryMethod: "fastest",
+            orderData: formData,
         }
 
         axios.post("/orders.json", order)
@@ -119,7 +116,7 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input
                         key={formElement.id}
@@ -128,7 +125,7 @@ class ContactData extends Component {
                         value={formElement.config.value}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button btnType="Success">ORDER</Button>
             </form>
         );
 
