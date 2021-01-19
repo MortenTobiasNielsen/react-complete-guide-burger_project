a1d7d86380cc28612f52ca92425f61352c3bd129
Morten <mtnielsen89@gmail.com>
2021-01-19 20:30:07 +0100

    Added logic to fetch ingredients data from a database into redux

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,26 +5,18 @@ import Burger from "./../../components/Burger/Burger";
 import BuildControls from "./../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
-import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler"
+import axios from "../../axios-orders";
 import * as burgerBuilderActions from "../../store/actions/index"
 
 class BurgerBuilder extends Component {
     state = {
         purchasing: false,
-        loading: false,
-        error: false,
     }
 
     componentDidMount () {
-        // axios.get("https://react-burger-builder-f166a-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json")
-        //     .then(response => {
-        //         this.setState({ ingredients: response.data });
-        //     })
-        //     .catch(error => {
-        //         this.setState({error: true})
-        //     });
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState (ingredients) {
@@ -107,7 +99,7 @@ class BurgerBuilder extends Component {
         }
 
         let orderSummary = null; 
-        let burger = this.state.error ? <p>Ingredients can't be loaded</p> : <Spinner/>
+        let burger = this.props.error ? <p>Ingredients can't be loaded</p> : <Spinner/>
 
         if (this.props.ings) {
             burger = (
@@ -132,10 +124,6 @@ class BurgerBuilder extends Component {
                 />
         }
 
-        if (this.state.loading) {
-            orderSummary = <Spinner />
-        }
-
         return (
             <React.Fragment>
                 <Modal 
@@ -153,6 +141,7 @@ const mapStateToProps = state => {
     return {
         ings: state.ingredients,
         totalPrice: state.totalPrice,
+        error: state.error,
     };
 };
 
@@ -160,6 +149,8 @@ const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
         onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
+        onFetchIngredientsFailed: () => dispatch(burgerBuilderActions.fetchIngredientsFailed()),
     };
 };
 
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,8 @@
 export {
     addIngredient,
     removeIngredient,
+    initIngredients, 
+    fetchIngredientsFailed,
 } from "./burgerBuilder";
 
 export {
